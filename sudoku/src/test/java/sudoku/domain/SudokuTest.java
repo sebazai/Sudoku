f5bafa5826040f4bd0bcf565181ee7a318dd44e0
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2019-03-26 15:53:21 +0200

    sudokutest.java

---
+++ sudoku/src/test/java/sudoku/domain/SudokuTest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sebserge
+ */
+public class SudokuTest {
+    Sudoku sudoku;
+    public SudokuTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        sudoku = new Sudoku(1);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void checkIfSafeMethodWorksAsFalse() {
+        int[][] fakeSudoku = new int[][]{
+            {1,2,3,4,5,6,7,8,9},
+            {4,5,6,7,8,9,1,2,3},
+            {7,8,9,1,2,3,4,5,6},
+            {0,0,0,0,0,0,0,0,0},
+            {0,9,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+        };
+        assertFalse(sudoku.checkIfSafe(fakeSudoku, 0, 4, 9));
+    }
+    
+    @Test
+    public void checkIfSafeMethodReturnsTrue() {
+        int[][] fakeSudoku = new int[][]{
+            {1,2,3,4,5,6,7,8,9},
+            {4,5,6,7,8,9,1,2,3},
+            {7,8,9,1,2,3,4,5,6},
+            {0,0,0,0,0,0,0,0,0},
+            {0,9,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+        };
+        assertTrue(sudoku.checkIfSafe(fakeSudoku, 8, 0, 6));
+    }
+
+    @Test
+    public void generateAndCheckSudoku() {
+        sudoku.generateSudoku();
+        boolean sudokuWorks = true;
+        // Go through the whole sudoku 9*9 and see if the generated sudoku is valid.
+        for(int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                // Take the number in the generated sudoku
+                int number = sudoku.playableSudoku[i][j];
+                // Assign it to zero and check if it can be placed there
+                sudoku.playableSudoku[i][j] = 0;
+                if(!sudoku.checkIfSafe(sudoku.playableSudoku, i, j, number)) sudokuWorks = false;
+            }
+        }
+        assertTrue(sudokuWorks);
+    }
+}
