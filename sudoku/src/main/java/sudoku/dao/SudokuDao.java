a9467ba9a9befddf4422626bb3b676343f29285b
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2019-04-18 21:42:41 +0300

    tietokanta käyttöön

---
+++ sudoku/src/main/java/sudoku/dao/SudokuDao.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku.dao;
+
+/**
+ *
+ * @author sebserge
+ */
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import sudoku.domain.Sudoku;
+
+public class SudokuDao {
+    private final String databasePath;
+
+    public SudokuDao(String databasePath) throws SQLException {
+        this.databasePath = databasePath;
+
+        Connection conn = DriverManager.getConnection(databasePath, "sa", "");
+        conn.prepareStatement(
+                "CREATE TABLE IF NOT EXISTS Sudoku ("
+                        + "id INTEGER AUTO_INCREMENT PRIMARY KEY, "
+                        + "initialboard VARCHAR(333), "
+                        + "playableboard VARCHAR(333), "
+                        + "solvedboard VARCHAR(333), "
+                        + "savedstamp TIMESTAMP WITH TIME ZONE"
+                        + ");").executeUpdate();
+
+        conn.close();
+    }
+
+    public void create(Sudoku sudoku) throws SQLException {
+        Connection conn = DriverManager.getConnection(databasePath, "sa", "");
+
+        PreparedStatement stmt = conn.prepareStatement(
+                "INSERT INTO Sudoku "
+                        + " (initialboard, playableboard, solvedboard, savedstamp) "
+                        + " VALUES (?, ?, ?, ?)");
+        stmt.setString(1, Arrays.deepToString(sudoku.getInitialSudoku()));
+        stmt.setString(2, Arrays.deepToString(sudoku.getPlayableSudoku()));
+        stmt.setString(3, Arrays.deepToString(sudoku.getSolvedSudoku()));
+        stmt.setTimestamp(4, new java.sql.Timestamp(new java.util.Date().getTime()));
+
+        stmt.executeUpdate();
+        stmt.close();
+        conn.close();
+    }
+
+    public void delete(int key) throws SQLException {
+        Connection conn = DriverManager.getConnection(databasePath, "sa", "");
+
+        PreparedStatement stmt = conn.prepareStatement(
+                "DELETE Sudoku WHERE id = ?");
+        stmt.setInt(1, key);
+
+        stmt.executeUpdate();
+        stmt.close();
+        conn.close();
+    }
+
+    public List<Sudoku> list() throws SQLException {
+        List<Sudoku> loadedSudokus = new ArrayList<>();
+        Connection conn = DriverManager.getConnection(databasePath, "sa", "");
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Sudoku LIMIT 5");
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            Sudoku sudokuLoad = new Sudoku(
+                    rs.getInt("id"),
+                    rs.getString("initialboard"),
+                    rs.getString("playableboard"),
+                    rs.getString("solvedboard"),
+                    rs.getTimestamp("savedstamp").toInstant());
+
+            loadedSudokus.add(sudokuLoad);
+        }
+
+        stmt.close();
+        conn.close();
+
+        return loadedSudokus;
+    }
+
+
+}
